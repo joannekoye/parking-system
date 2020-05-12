ee11f7c3acd7fc2c24e5bc20c49eea8a5a8fe79b
Joan Nekoye <joannekoye@gmail.com>
2020-05-12 16:02:17 +0300

    create flask boilerplate

---
+++ config.py
@@ -0,0 +1,32 @@
+ 
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = os.environ.get('DEBUG')
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+config_options = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing':TestingConfig
+}
