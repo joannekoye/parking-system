4ef0d969739a7c8ed77b2fd4534342610793a69c
ow-tech <wagwanwekon@outloo.com>
2020-05-13 14:57:32 +0300

    adding of flash message and errors for inputs

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,16 +1,19 @@
 from . import auth
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 # from ..models import
 from .forms import RegistrationForm, LoginForm
 # from .. import db
 
-@auth.route("/login")
+@auth.route("/login"methods=['GET','POST'])
 def login():
     form = LoginForm()
     return render_template('auth/login.html', title='login', form=form)
 
 
-@auth.route("/register")
+@auth.route("/register",methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('auth.login'))
     return render_template('auth/register.html', title='Register', form=form)
