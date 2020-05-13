d4b30b7dd316dd69c195616ba8962f67d95d5d62
ow-tech <wagwanwekon@outloo.com>
2020-05-13 12:39:29 +0300

    chore; changing of forms location to auth folder and importing forms

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,8 @@
 from . import auth
+from flask import render_template, redirect, url_for
+# from ..models import
+from .forms import RegistrationForm, LoginForm
+# from .. import db
 
 @auth.route("/login")
 def login():
