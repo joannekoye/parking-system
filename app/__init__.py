ff08a856a2583902ee0d81dc8bfaaedacbf3bab4
Joan Nekoye <joannekoye@gmail.com>
2020-05-14 20:58:53 +0300

    fix merge conflict

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,19 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from flask_bcrypt import Bcrypt
+from flask_marshmallow import Marshmallow
 from flask_login import LoginManager
-
-
+from flask_bcrypt import Bcrypt
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.session_protection='strong'
+login_manager.login_manager_category = 'info'
 db = SQLAlchemy()
 bootstrap = Bootstrap()
 bcrypt = Bcrypt()
-login_manager = LoginManager()
-login_manager.login_view='auth.login'
-login_manager.login_manager_category = 'info'
+ma = Marshmallow()
+
+
 # from app.models import User
 
 # create an application factory
@@ -38,13 +41,18 @@ def create_app(config_name):
     bootstrap.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    ma.init_app(app)
 
     # register your blueprints here
-    from app.main import main
-    from app.auth import auth
+    from app.auth import auth as auth_blueprint
+    from app.main import main as main_blueprint
+    from .parking import parking as parking_blueprint
+    from .admin import admin as admin_blueprint
 
-    app.register_blueprint(main)
-    app.register_blueprint(auth)
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(parking_blueprint,url_prefix='/parking')
+    app.register_blueprint(admin_blueprint,url_prefix='/admin')
 
 
     return app
--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
 export SECRET_KEY="insert secret key"
 export DATABASE_URL="insert your db url"
 python3.6 manage.py server
+
+
+app/__init__.py
+app/auth/forms.py
+app/auth/views.py
+app/models.py
+app/static/css/index.css
+app/templates/auth/login.html
+app/templates/auth/register.html
+manage.py
+requirements.txt
+start.sh
